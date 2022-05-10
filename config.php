5c651ede13630a218dbd4524f55ba97184788e2b
RElSLIMA <reislima2005@gmail.com>
2022-05-09 21:31:19 -0300

    1

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$db_name = "myslq:host=localhost;dbname=aqui_tem_peca_db";
+$username = "root";
+$password = "";
+
+$conn = new PDO($db_name, $username, $password );
+
+
+
+
+
+
+
+?>
--- registro.php
+++ registro.php
@@ -1,4 +1,15 @@
+<?php
+
+include 'config.php'
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
+
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
